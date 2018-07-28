dca3d45ae793f18a9482ea82e80659e25f71b0dd
Zachary Lytle <lytle.zack@gmail.com>
2018-07-28 11:35:15 -0400

    Cleanup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import logo from './logo.gif'
 import './Header.css'
 
-const Header = () => {
+const  Header = () => {
     return (
         <header className='header'>
-            <img src={logo} className='logo' alt='' />
-            <h1>A Lytle EOS Explorer</h1>
-        </header>
+            <img src={logo} className='logo' alt=''/>
+                <h1>A Lytle EOS Explorer</h1>
+            </header>
     )
-}
+};
 
 export default Header;
